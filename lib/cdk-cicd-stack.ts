5703e6975b1525b40ea0f0a401984652396070d6
rossislike <ross.agginie@gmail.com>
2026-02-23 09:35:28 -0500

    Add source

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,24 @@
 import * as cdk from "aws-cdk-lib/core"
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines"
 import { Construct } from "constructs"
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    new CodePipeline(this, "AwesomePipeline", {
+      pipelineName: "AwesomePipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "rossislike/cdk-cicd",
+          "cicd-practice",
+        ),
+        commands: ["npm ci", "npx cdk synth"],
+      }),
+    })
   }
 }
