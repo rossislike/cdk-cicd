e67d7995aca75ae7160ca3d9086ab5fb8acf32f4
rossislike <ross.agginie@gmail.com>
2026-03-01 06:50:04 -0500

    Add unit tests

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib/core"
 import {
+  CodeBuildStep,
   CodePipeline,
   CodePipelineSource,
   ShellStep,
@@ -25,6 +26,12 @@ export class CdkCicdStack extends cdk.Stack {
     const testStage = pipeline.addStage(
       new PipelineStage(this, "PipelineTestStage", {
         stageName: "test",
+      }),
+    )
+
+    testStage.addPre(
+      new CodeBuildStep("unit-tests", {
+        commands: ["npm ci", "npm test"],
       }),
     )
   }
