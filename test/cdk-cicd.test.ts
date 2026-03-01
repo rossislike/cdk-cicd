6abb10a7e45b0680184046efea6968585810816b
rossislike <ross.agginie@gmail.com>
2026-03-01 07:27:50 -0500

    Fix test

--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -3,6 +3,6 @@ import { handler } from "../services/hello"
 describe("Hello describe test suite", () => {
   test("handler should return 200", async () => {
     const result = await handler({}, {})
-    expect(result.statusCode).toBe(400)
+    expect(result.statusCode).toBe(200)
   })
 })
