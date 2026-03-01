1e8439ea0a5aeb143e896f5e62c3a6cbb39f605c
rossislike <ross.agginie@gmail.com>
2026-03-01 06:19:23 -0500

    Actually add the stage

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -5,12 +5,13 @@ import {
   ShellStep,
 } from "aws-cdk-lib/pipelines"
 import { Construct } from "constructs"
+import { PipelineStage } from "./PipelineStage"
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    new CodePipeline(this, "AwesomePipeline", {
+    const pipeline = new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwesomePipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub(
@@ -20,5 +21,11 @@ export class CdkCicdStack extends cdk.Stack {
         commands: ["npm ci", "npx cdk synth"],
       }),
     })
+
+    const testStage = pipeline.addStage(
+      new PipelineStage(this, "PipelineTestStage", {
+        stageName: "test",
+      }),
+    )
   }
 }
